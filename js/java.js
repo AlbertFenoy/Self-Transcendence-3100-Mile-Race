9e42d510555d4c0c313a5b2db985521d05ec06bf
AlbertFenoy <100006439.joan23@fje.edu>
2020-12-10 13:05:14 +0100

    1

---
+++ js/java.js
@@ -0,0 +1,72 @@
+window.onload = function() {
+    $('#Onload').fadeOut();
+};
+(function() {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let birthday = "Sep 13, 2021 12:00:00",
+        countDown = new Date(birthday).getTime(),
+        x = setInterval(function() {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText = Math.floor(distance / (day)),
+                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+            //do something later when date is reached
+            if (distance < 0) {
+                let headline = document.getElementById("headline"),
+                    countdown = document.getElementById("countdown"),
+                    content = document.getElementById("content");
+
+                headline.innerText = "It's my birthday!";
+                countdown.style.display = "none";
+                content.style.display = "block";
+
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
+}());
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+        document.getElementById("nav").style.backgroundColor = "rgb(95, 196, 230)";
+    } else {
+        document.getElementById("nav").style.backgroundColor = "rgba(0,0,0,0.0)";
+    }
+};
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
