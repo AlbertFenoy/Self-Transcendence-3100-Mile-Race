e9dc5a3e1ad1daf7f7b525d7ba4d50007fc38ef3
AlbertFenoy <100006439.joan23@fje.edu>
2020-12-16 17:32:14 +0100

    F1.0

--- js/java.js
+++ js/java.js
@@ -1,3 +1,4 @@
+//Contador
 (function() {
     const second = 1000,
         minute = second * 60,
@@ -16,7 +17,6 @@
                 document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
                 document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-            //do something later when date is reached
             if (distance < 0) {
                 let headline = document.getElementById("headline"),
                     countdown = document.getElementById("countdown"),
@@ -28,14 +28,13 @@
 
                 clearInterval(x);
             }
-            //seconds
         }, 0)
 }());
 
+//Scroll
 window.onscroll = function() {
     scrollFunction()
 };
-
 function scrollFunction() {
     if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
         document.getElementById("nav").style.backgroundColor = "rgb(95, 196, 230)";
@@ -44,26 +43,19 @@ function scrollFunction() {
     }
 };
 $(document).ready(function() {
-    // Add smooth scrolling to all links
     $("a").on('click', function(event) {
 
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
             event.preventDefault();
 
-            // Store hash
             var hash = this.hash;
 
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 800, function() {
 
-                // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
-        } // End if
+        }
     });
 });
